7cc69f7b44face69f5be561a03367a77f3872298
yehiahesham2938 <s-yehia.abdelrasol@zewailcity.edu.eg>
2025-04-26 21:23:08 +0300

    refactor: rename salting and hashing functions to match assignment requirements

--- password-utils.js
+++ password-utils.js
@@ -1,17 +1,17 @@
 const crypto = require('crypto');
 
 function hashPassword(password) {
-  const randomString = crypto.randomBytes(16).toString('hex');
-  const hash = crypto.pbkdf2Sync(password, randomString, 100000, 64, 'sha512').toString('hex');
-  return randomString + ':' + hash;
+  const salt = crypto.randomBytes(16).toString('hex');
+  const hash = crypto.pbkdf2Sync(password, salt, 100000, 64, 'sha512').toString('hex');
+  return salt + ':' + hash;
 }
 
 function verifyPassword(password, storedHash) {
   const parts = storedHash.split(':');
-  const secretAddon = parts[0];
+  const salt = parts[0];
   const originalHash = parts[1];
   
-  const newHash = crypto.pbkdf2Sync(password, secretAddon, 100000, 64, 'sha512').toString('hex');
+  const newHash = crypto.pbkdf2Sync(password, salt, 100000, 64, 'sha512').toString('hex');
   
   return newHash === originalHash;
 }
